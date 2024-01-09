c617abe0c103f88b67207f87c01ace9f60101318
Alex Baibachenko <alexbajbachenko2@gmail.com>
2024-01-09 12:41:34 +0200

    fix: fix ContentLabel classes

--- src/components/Content/ContentLabel.js
+++ src/components/Content/ContentLabel.js
@@ -1,4 +1,10 @@
 const ContentLabel = (label) => {
-  return <div className={label.style}>{label.value}</div>;
+  return (
+    <div
+      className={`user_block__table_label user_block__table_label--${label.style}-mod`}
+    >
+      {label.value}
+    </div>
+  );
 };
 export default ContentLabel;
